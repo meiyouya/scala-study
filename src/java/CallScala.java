167ede885d16a132262fc2e3e95e57b06e496ab6
lawliet <837184598@qq.com>
2021-03-18 17:40:12 +0800

    java call scala

---
+++ src/java/CallScala.java
@@ -0,0 +1,8 @@
+import grammar.ConditionController;
+
+public class CallScala {
+    public static void main(String[] args) {
+        ConditionController conditionController = new ConditionController();
+        System.out.println(conditionController.call());
+    }
+}
