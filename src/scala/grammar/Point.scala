d55b01c6e022478815a2005ad29c0bbf55b009ed
lawliet <837184598@qq.com>
2021-03-23 18:41:14 +0800

    class extends

--- src/scala/grammar/Point.scala
+++ src/scala/grammar/Point.scala
@@ -14,9 +14,24 @@ class Point(xInit: Int, yInit: Int) {
 
 }
 
+class Location(xInit: Int, yInit: Int, zInit: Int) extends Point(xInit, yInit) {
+
+    var z: Int = zInit
+
+    def move(xAdd: Int, yAdd: Int, zAdd: Int): Unit = {
+        x += xAdd
+        y += yAdd
+        z += zAdd
+        println("移动后的坐标：(" + x + ", " + y + ", " + z + ")")
+    }
+}
+
 object Test {
     def main(args: Array[String]): Unit = {
         val point = new Point(10, 10)
         point.move(5, 10)
+        val location = new Location(5, 5, 5)
+        location.move(1, 2)
+        location.move(3, 4, 5)
     }
 }
